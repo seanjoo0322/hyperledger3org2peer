65869c738cfeffb4f0df76c8babbbb55e8751bb2
Hyun Seo Joo(Sean Joo) <112134402+seanjoo0322@users.noreply.github.com>
2023-12-11 13:16:29 +0900

    Add files via upload

---
+++ 3orderer4peer/scripts/peerAdd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CHANNEL_NAME="$1"
+DELAY="$2"
+MAX_RETRY="$3"
+VERBOSE="$4"
+BFT="$5"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${MAX_RETRY:="5"}
+: ${VERBOSE:="false"}
+: ${BFT:=0}
+
+: ${CONTAINER_CLI:="docker"}
+: ${CONTAINER_CLI_COMPOSE:="${CONTAINER_CLI}-compose"}
+infoln "Using ${CONTAINER_CLI} and ${CONTAINER_CLI_COMPOSE}"
